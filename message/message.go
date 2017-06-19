dc0931a8728a119ebdf0406042d697c5353efafd
DeveloppSoft <developpsoft@gmail.com>
2017-06-19 09:49:14 +0200

    [message] Add convert to bytes

--- message/message.go
+++ message/message.go
@@ -1,6 +1,7 @@
 package message
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -18,4 +19,9 @@ type Message struct {
 
 func (m *Message) IsValid() bool {
 	return true
+}
+
+func (m *Message) Bytes() []byte {
+	// Small hack
+	return fmt.Sprintf("%v", m)
 }

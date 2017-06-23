ae7a941cbb2595699fb7b28b21ef46d5c15b74ed
DeveloppSoft <developpsoft@gmail.com>
2017-06-23 09:16:17 +0200

    [pqcrypto] Remove logger

--- pqcrypto/rng.go
+++ pqcrypto/rng.go
@@ -5,13 +5,10 @@ import (
 	"errors"
 
 	rand "github.com/DeveloppSoft/go-rand"
-	log "github.com/inconshreveable/log15"
 )
 
 var (
 	ErrBytes = errors.New("didn't got enough bytes")
-
-	logger_rng = log.New("pqcrypto", "rng")
 )
 
 // Return 'nb' random bytes, thanks to chacha20
@@ -20,19 +17,16 @@ func GetRandom(nb int) ([]byte, error) {
 
 	r, err := rand.New(seed[:])
 	if err != nil {
-		logger_rng.Error(err.Error())
 		return nil, err
 	}
 
 	var buffer = make([]byte, nb)
 	n, err := r.Read(buffer[:])
 	if err != nil {
-		logger_rng.Error(err.Error())
 		return nil, err
 	}
 
 	if n != len(buffer) {
-		logger_rng.Error(ErrBytes.Error())
 		return nil, ErrBytes
 	}
 

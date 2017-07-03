5a8bdf4dece955c0a74d4c0afcc34df3bc8499fc
DeveloppSoft <developpsoft@gmail.com>
2017-07-03 14:13:46 +0200

    [repo] Extend interface

---
+++ repo/about.go
@@ -0,0 +1,24 @@
+package repo
+
+import (
+	"encoding/json"
+)
+
+type About struct {
+	Pseudo      string
+	Image       string
+	ETHAddress  string
+	Description string
+}
+
+func (a *About) Export() ([]byte, error) {
+        return json.Marshal(a)
+}
+
+func Import(b []byte) (About, error) {
+        var a About
+        err := json.Unmarshal(b, &a)
+
+        return a, err
+}
+

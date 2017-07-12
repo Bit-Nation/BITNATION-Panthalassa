77d41374d443a06008d4053200cf188bde4e1bfe
DeveloppSoft <developpsoft@gmail.com>
2017-07-12 17:04:02 +0200

    [repo] fix message uploading

--- repo/util.go
+++ repo/util.go
@@ -2,10 +2,17 @@ package repo
 
 import (
 	"os"
+	"io/ioutil"
 )
 
 func checkAndMake(path string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		os.MkdirAll(path, os.ModePerm) // TODO: better mode
+	}
+}
+
+func checkAndMakeFile(path string, content []byte) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		ioutil.WriteFile(path, content, os.ModePerm)
 	}
 }
